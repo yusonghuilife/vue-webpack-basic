a42a779ffdcd5ea9f0341960a4a5e9a47e0d3592
yusonghu <yusong1209@gmail.com>
2020-03-22 15:51:44 +0800

    feat: init

---
+++ build/webpack.dev.js
@@ -0,0 +1,54 @@
+const merge = require('webpack-merge')
+const webpackConfig = require('./webpack.config')
+const webpack = require('webpack')
+module.exports = merge(webpackConfig, {
+  mode: 'development',
+  devtool: 'cheap-module-eval-source-map',
+  module: {
+    rules: [
+      // 遇到后缀为.css的文件，webpack先用css-loader加载器去解析这个文件，遇到“@import”“url”等语句就将相应样式文件引入（所以如果没有css-loader，就没法解析这类语句），最后计算完的css，将会使用style-loader生成一个内容为最终解析完的css代码的style标签，放到head标签里。
+      // 需要注意的是，loader是有顺序的，webpack肯定是先将所有css模块依赖解析完得到计算结果再创建style标签。因此应该把style-loader放在css-loader的前面
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+          }
+        ]
+      },
+      {
+        test: /\.(scss|sass)$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 2
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              implementation: require('dart-sass')
+            }
+          },
+          {
+            loader: 'postcss-loader'
+          }
+        ]
+      },
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('development')
+      }
+    }),
+  ]
+})
